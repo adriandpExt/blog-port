7e6f4718e5ab91681c0a3c312def4c1fbf136bc9
adriandpExt <adriandp.ext@gmail.com>
2024-07-07 13:25:47 +0800

    added project

--- src/screens/works/utils.ts
+++ src/screens/works/utils.ts
@@ -1,6 +1,8 @@
 import empf1 from "~/assets/empf/empf1.png";
 import shop1 from "~/assets/shop/shop1.png";
 import lps from "~/assets/images/lenovo-pccw.png";
+import taskDash from "~/assets/project3/task-dashboard.png";
+import pokemonCard from "~/assets/project3/pokemon-card.png";
 
 export const projectsList = [
   {
@@ -14,16 +16,14 @@ export const projectsList = [
     tech: ["React", "Typescript", "Json-Server", "Tailwind", "Daisy UI"],
   },
   {
-    image:
-      "https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg",
-    title: "Coming Soon!",
-    tech: ["React", "Typescript", "Json-Server", "Tailwind"],
+    image: taskDash,
+    title: "Task Tracker",
+    tech: ["React", "Javascript", "Node Js", "Express TS", "Tailwind"],
   },
   {
-    image:
-      "https://img.freepik.com/free-vector/coming-soon-text-grunge-background_91128-1643.jpg",
-    title: "Coming Soon!",
-    tech: ["React", "Typescript", "Json-Server", "Tailwind"],
+    image: pokemonCard,
+    title: "Pokemon Flip Card Game",
+    tech: ["React", "Javascript", "Tailwind", "Daisy UI", "Axios"],
   },
   {
     image:
